78dd95749a00a887e30122f275c51658d0a60f5e
Pixxiies <pa.pongsapak_st@tni.ac.th>
2015-12-01 17:01:26 +0700

    Gu Kok TUM

---
+++ Package.py
@@ -0,0 +1,13 @@
+class Package(object):
+
+	def __init__(self,ID,Name,Price,Discount,PPT):
+	    self.ID = ID
+	    self.Name = Name
+	    self.Price = Price
+	    self.Discount = Discount
+	    self.PPT = PPT
+	    self.Date = Date
+	
+
+
+
